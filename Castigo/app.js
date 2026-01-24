bbe5b7f4a6b0406d0a877833b3111c3d5b59acfa
emilianux <al07108460@tecmilenio.mx>
2026-01-23 21:52:45 -0600

    Finalizacion de ejercicios de castigo

--- Castigo/app.js
+++ Castigo/app.js
@@ -110,6 +110,107 @@ formatear.addEventListener("click", () => {
 
 
 // EJERCICIO 6
+const numMinutos = document.getElementById("numMinutos");
+const tiempoResultado = document.getElementById("tiempoResultado");
+const btnConvertir = document.getElementById("btnConvertir");
+
+const convertirMinutos = (totalMinutos) => {
+    const minutos = totalMinutos % 60;
+    const horas = Math.floor(totalMinutos / 60);
+    tiempoResultado.innerHTML = `${horas}h ${minutos}m`;
+}
+
+btnConvertir.addEventListener("click", () => {
+    const valor = parseInt(numMinutos.value);
+    if (valor < 0) {
+        alert("Error: Ingresa un numero de minutos valido");
+    } else if (!isNaN(valor)){
+        convertirMinutos(valor);
+    } else {
+        alert("Error: Debes ingresar un numero.");
+    }
+});
+
+// EJERCICIO 7
+const email = document.getElementById("email");
+const btnValidar = document.getElementById("btnValidar");
+const emailResultado = document.getElementById("emailResultado");
+
+btnValidar.addEventListener("click", () => {
+    if(email.value === ""){
+        emailResultado.innerHTML = "Ingresa un email";
+    } else if (!email.value.includes("@")){
+        emailResultado.innerHTML = "El email no es valido";
+    } else {
+        emailResultado.innerHTML = "El email es valido";
+    }
+})
+
+// EJERCICIO 8
+const numProm1 = document.getElementById("numProm1");
+const numProm2 = document.getElementById("numProm2");
+const numProm3 = document.getElementById("numProm3");
+const btnCalcularProm = document.getElementById("btnCalcularProm");
+const promResultado = document.getElementById("promResultado");
+
+btnCalcularProm.addEventListener("click", () => {
+    const prom = (parseFloat(numProm1.value) + parseFloat(numProm2.value) + parseFloat(numProm3.value)) / 3;
+
+    if (prom > 100 || prom < 0){
+        alert("Las calificaciones deben estar entre 0 y 100");
+    } else if (prom >= 70) {
+        promResultado.innerHTML = prom.toFixed(2) + " (Aprobado)";
+    } else if (prom < 70) {
+        promResultado.innerHTML = prom.toFixed(2) + " (Reprobado)";
+    } else {
+        promResultado.innerHTML = "xd";
+    }
+});
+
+
+// EJERCICIO 9
+const textoConvertir = document.getElementById("textoConvertir");
+const btnMayusculas = document.getElementById("btnMayusculas");
+const btnMinusculas = document.getElementById("btnMinusculas");
+const btnLongitud = document.getElementById("btnLongitud");
+const resultadoConvertir = document.getElementById("resultadoConvertir");
+
+btnMayusculas.addEventListener("click", () => {
+    resultadoConvertir.innerHTML = textoConvertir.value.toUpperCase();
+});
+
+btnMinusculas.addEventListener("click", () => {
+    resultadoConvertir.innerHTML = textoConvertir.value.toLowerCase();
+});
+
+btnLongitud.addEventListener("click", () => {
+    const textoLong = textoConvertir.value.length;
+    resultadoConvertir.innerHTML = `Longitud: ${textoLong} caracteres`
+});
+
+
+// EJERCICIO 10
+const precio = document.getElementById("precio");
+const descuento = document.getElementById("descuento");
+const btnCalcularDescuento = document.getElementById("btnCalcularDescuento");
+const descuentoResultado = document.getElementById("descuentoResultado");
+
+btnCalcularDescuento.addEventListener("click", () => {
+    const precioFinal = precio.value - (precio.value * descuento.value / 100);
+
+    if (precio.value <= 0){
+        alert("El precio debe ser mayor a 0");
+    } else if (descuento.value > 100 || descuento.value < 0){
+        alert("El descuento debe estar entre 0 y 100");
+    } else {
+        descuentoResultado.innerHTML = `Precio original: ${precio.value}. <br> Descuento: ${descuento.value}% <br> Precio final: ${precioFinal.toFixed(2)}`
+    }
+})
+
+
+
+
+
 
 
 
